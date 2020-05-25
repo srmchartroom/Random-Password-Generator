858021c8329a96b79fb5aafb0780b600d04054e3
srmchartroom <sean@chartroomcreative.com>
2020-05-25 03:42:45 -0400

    Cleaned up comments on Password Length section. Added missed ; for pwLength prompt.

--- Develop/script.js
+++ Develop/script.js
@@ -28,25 +28,28 @@ const arrSpecial = ["!","\"","#","$","%","&","'","(",")","*","+","-","/","\\",":
 // Create function 'generatePassword();' to run when button clicked.
 function generatePassword() {
 
-  // *** PASSWORD LENGTH: **** //
-  
-  // Set a variable and assign it to the user-defined password length from prompt.
-  const pwLength = prompt("Set your Password Length between 8 and 128 characters.")
-  
-  // Since prompts auto-convert entry to strings, convert the length to a number.
-  parseInt(pwLength);
-
-  // Log the entered length to console for checking/error-handling.
-  console.log("Length: " + pwLength);
+  // ** PASSWORD LENGTH: ** //
+  const pwLength = prompt("Set your Password Length between 8 and 128 characters."); // Set a variable to the user-defined password length from prompt.
+  parseInt(pwLength); // Since prompts auto-convert to string, converts length to a #.
+  console.log("Length: " + pwLength); // Logs length to console for checking/errors
   
   // Check if 8 >= pwLength >= 128. Return alert message to redo if not.
-  if (pwLength < 8) {
-    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again.");
-    return "Please try again.";
-  } else if (pwLength > 128) {
-    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again.");
-    return "Please try again.";
-  } else {}
+  if (pwLength < 8) { // Checking for < 8. Returns an error alert.
+    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again."); // Show the use an error alert.
+    return "Please try again."; // Show a try-again message in the pw text area.
+  } else if (pwLength > 128) { // Checking for > 128. Returns an error alert.
+    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again."); // Show the use an error alert.
+    return "Please try again."; // Show a try-again message in the pw text area.
+  } else { // Length is OK. Allows the function to continue...
+
+    // ** CHARACTER TYPES SELECTION ** //
+
+
+
+
+
+
+  }
 
   // CHARACTER TYPES:
   // Set up character type confirmations if password length is OK.
