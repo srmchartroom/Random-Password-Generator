77dcc2f5435ba9bd10666f814684bb679c2c8dcd
srmchartroom <sean@chartroomcreative.com>
2020-05-26 02:31:26 -0400

    Created scenario 5 (lowercase, numbers, and special characters, but not uppercase letters) and scenario 6 (lowercase letters only, not uppers, numbers, or special characters).

--- Develop/script.js
+++ Develop/script.js
@@ -170,6 +170,42 @@ function generatePassword() {
       return pwResult;
     } // end of scenario 4  
     // Scenario 5: L=T, U=F, N=T, $=T
+    else if (pwLower == true && pwUpper !== true && pwNum == true && pwSpecial == true) {
+      console.log("Scenario 5: Lowercase, Numbers, Special Characters - BUT NO UPPERCASE LETTERS");
+      let pwArray = [0];
+      for(var i = 0; i < pwLength; i++) {
+        let typeIndex = Math.floor(Math.random() * 3);
+        if (typeIndex == 0) {
+          let iL = arrLetters[Math.floor(Math.random() * 26)];
+          pwArray[i] = iL;
+        } else if (typeIndex == 1) {
+          let iN = [Math.floor(Math.random() * 10)];
+          pwArray[i] = iN;
+        } else {
+          let iS = arrSpecial[Math.floor(Math.random() * 29)];
+          pwArray[i] = iS;
+        } 
+        console.log("Character " + (i+1) + ": " + pwArray[i]);
+      }
+      pwResult = pwArray.join("");
+      console.log("Final Password: " + pwResult);
+      return pwResult;
+    } // end of scenario 5
+    // Scenario 6: L=T, U=F, N=F, $=F
+    else if (pwLower == true && pwUpper !== true && pwNum !== true && pwSpecial !== true) {
+      console.log("Scenario 6: Lowercase only - NO UPPERCASE, NO NUMBERS, NO SPECIAL CHARACTERS");
+      let pwArray = [0];
+      for(var i = 0; i < pwLength; i++) {
+        let iL = arrLetters[Math.floor(Math.random() * 26)];
+        pwArray[i] = iL;
+        console.log("Character " + (i+1) + ": " + pwArray[i]);
+      }
+      pwResult = pwArray.join("");
+      console.log("Final Password: " + pwResult);
+      return pwResult;
+    } // end of scenario 6
+    
+
 
   } // end of else     
 
