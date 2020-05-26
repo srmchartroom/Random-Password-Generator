697d428542579a5e8ef27b623ece65431cdbc0f3
srmchartroom <sean@chartroomcreative.com>
2020-05-26 01:30:17 -0400

    Created scenario 3: lowercase, uppercase, and special characters - but not numbers.

--- Develop/script.js
+++ Develop/script.js
@@ -119,13 +119,35 @@ function generatePassword() {
           let iU = arrLetters[Math.floor(Math.random() * 26)].toUpperCase();
           pwArray[i] = iU;
         }
-      console.log("Character " + (i+1) + ": " + pwArray[i]);
+        console.log("Character " + (i+1) + ": " + pwArray[i]);
       }
-    pwResult = pwArray.join("");
-    console.log("Final Password: " + pwResult);
-    return pwResult;
-   }
-    
+      pwResult = pwArray.join("");
+      console.log("Final Password: " + pwResult);
+      return pwResult;
+    } // end of scenario 2
+    // Scenario 3: L=T, U=T, N=F, $=T
+    else if (pwLower == true && pwUpper == true && pwNum !== true && pwSpecial == true) {
+      console.log("Case 3: Lowercase, Uppercase, Special Characters - BUT NO NUMBERS");
+      let pwArray = [0];
+      for(var i = 0; i < pwLength; i++) {
+        let typeIndex = Math.floor(Math.random() * 3);
+        if (typeIndex == 0) {
+          let iL = arrLetters[Math.floor(Math.random() * 26)];
+          pwArray[i] = iL;  
+        } else if (typeIndex == 1) {
+          let iU = arrLetters[Math.floor(Math.random() * 26)].toUpperCase();
+          pwArray[i] = iU;
+        } else if (typeIndex == 2) { 
+          let iS = arrSpecial[Math.floor(Math.random() * 29)];
+          pwArray[i] = iS;
+        }
+        console.log("Character " + (i+1) + ": " + pwArray[i]);
+      }
+      pwResult = pwArray.join("");
+      console.log("Final Password: " + pwResult);
+      return pwResult;
+    } // end of scenario 3
+     
 
   } // end of else     
 
