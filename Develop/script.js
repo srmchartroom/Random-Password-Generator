ae076146856124501403d340b77822328ce147b4
srmchartroom <sean@chartroomcreative.com>
2020-05-26 01:16:31 -0400

    Created scenario 1: if lowercase, uppercase, numbers, and special characters are all used. Created random character type selection to level odds of any one of the character type.  Set for loop to handle character creation across each index position in a password array, generated characters based on whether the character type random selection was uppercase, lowercase, number, or special character. Created final var to hold a merged & joined password based on the full array of characters, and then returned the resulting password string to then writePassword() function. BIG COMMIT here.  Lots added in the described changes here.  Also commented per line to clarify for downstream branching and cloning devs so can see what the code is doing at each point.

--- Develop/script.js
+++ Develop/script.js
@@ -16,9 +16,9 @@ generateBtn.addEventListener("click", writePassword);
 
 // **GLOBAL VARIABLES** //
 
-let pwResult = "";  // Setting a placeholder for the final function result
-const arrLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];  // Setting up arrays for character type selection: Letters
-const arrSpecial = ["!","\"","#","$","%","&","'","(",")","*","+","-","/","\\",":",";",">","<","=","?","@","[","]","^","_","{","}","|","~"];  // Setting up arrays for character type selection: Special Characters
+let pwResult = "";    // Setting a placeholder for the final function result
+const arrLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];   // Setting up arrays for character type selection: Letters
+const arrSpecial = ["!","\"","#","$","%","&","'","(",")","*","+","-","/","\\",":",";",">","<","=","?","@","[","]","^","_","{","}","|","~"];   // Setting up arrays for character type selection: Special Characters
 
 // ** PASSWORD GENERATION FUNCTION ** //
 
@@ -27,37 +27,37 @@ function generatePassword() {
   
   // ** PASSWORD LENGTH: ** //
   
-  const pwLength = prompt("Set your Password Length between 8 and 128 characters."); // Set a variable to the user-defined password length from prompt.
-  parseInt(pwLength); // Since prompts auto-convert to string, converts length to a #.
-  console.log("Length: " + pwLength); // Logs length to console for checking/errors
+  const pwLength = prompt("Set your Password Length between 8 and 128 characters.");    // Set a variable to the user-defined password length from prompt.
+  parseInt(pwLength);   // Since prompts auto-convert to string, converts length to a #.
+  console.log("Length: " + pwLength);   // Logs length to console for checking/errors
   
   // Check if 8 >= pwLength >= 128. Return alert message to redo if not.
-  if (pwLength < 8) { // Checking for < 8. Returns an error alert.
-    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again."); // Show the use an error alert.
-    return "Please try again."; // Show a try-again message in the pw text area.
-  } else if (pwLength > 128) { // Checking for > 128. Returns an error alert.
-    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again."); // Show the use an error alert.
-    return "Please try again."; // Show a try-again message in the pw text area.
-  } else { // Length is OK. Allows the function to continue...
+  if (pwLength < 8) {   // Checking for < 8. Returns an error alert.
+    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again.");    // Show the use an error alert.
+    return "Please try again.";   // Show a try-again message in the pw text area.
+  } else if (pwLength > 128) {    // Checking for > 128. Returns an error alert.
+    alert("Password does not meet the length criteria. Please select the \"Generate Password\" button to try again.");    // Show the use an error alert.
+    return "Please try again.";   // Show a try-again message in the pw text area.
+  } else {    // Length is OK. Allows the function to continue...
 
     // ** CHARACTER TYPES SELECTION ** //
     
     // Lowercase letters character type:
-    const pwLower = confirm("Use lowercase letters in the password? Select \"OK\" to use lowercase letters. Select \"Cancel\" to continue."); // Set a variable for lowercase letters to a confirm() message Boolean result
-    console.log(""); // Add a space in the console output between length and confirm result
-    console.log("Lowercase: " + pwLower); // Logs Lowercase? T/F confirm result to console for checking/errors
+    const pwLower = confirm("Use lowercase letters in the password? Select \"OK\" to use lowercase letters. Select \"Cancel\" to continue.");   // Set a variable for lowercase letters to a confirm() message Boolean result
+    console.log("");    // Add a space in the console output between length and confirm result
+    console.log("Lowercase: " + pwLower);   // Logs Lowercase? T/F confirm result to console for checking/errors
     
     // Uppercase letters character type:
-    const pwUpper = confirm("Use UPPERCASE letters in the password? Select \"OK\" to use UPPERCASE letters. Select \"Cancel\" to continue."); // Set a variable for uppercase letters to a confirm() message Boolean result
-    console.log("Uppercase: " + pwUpper); // Logs Uppercase? T/F confirm result to console for checking/errors
+    const pwUpper = confirm("Use UPPERCASE letters in the password? Select \"OK\" to use UPPERCASE letters. Select \"Cancel\" to continue.");   // Set a variable for uppercase letters to a confirm() message Boolean result
+    console.log("Uppercase: " + pwUpper);   // Logs Uppercase? T/F confirm result to console for checking/errors
     
     // Numbers character type:
-    const pwNum = confirm("Use numbers (e.g. 0, 2, 7, etc.) in the password? Select \"OK\" to use numbers. Select \"Cancel\" to continue."); // Set a variable for numbers to a confirm() message Boolean result
-    console.log("Number: " + pwNum); // Logs Number? T/F confirm result to console for checking/errors 
+    const pwNum = confirm("Use numbers (e.g. 0, 2, 7, etc.) in the password? Select \"OK\" to use numbers. Select \"Cancel\" to continue.");    // Set a variable for numbers to a confirm() message Boolean result
+    console.log("Number: " + pwNum);    // Logs Number? T/F confirm result to console for checking/errors 
 
     // Special Characters character type:
-    const pwSpecial = confirm("Use special characters (e.g. @, ^, $, etc.) in the password? Select \"OK\" to use special characters. Select \"Cancel\" to continue."); // Set a variable for special chars to a confirm() message Boolean result
-    console.log("Number: " + pwNum); // Logs Number? T/F confirm result to console for checking/errors 
+    const pwSpecial = confirm("Use special characters (e.g. @, ^, $, etc.) in the password? Select \"OK\" to use special characters. Select \"Cancel\" to continue.");    // Set a variable for special chars to a confirm() message Boolean result
+    console.log("Number: " + pwNum);    // Logs Number? T/F confirm result to console for checking/errors 
 
     // ** PASSWORD SELECTION SCENARIOS ** //
     
@@ -80,6 +80,33 @@ function generatePassword() {
         // Error Handle Scenario 16: Must select 1 character type //
       Scenario 16: pwLower: F, pwUpper: F, pwNum: F, pwSpecial: F  */ 
 
-    
+    // Scenario 1: L=T, U=T, N=T, $=T
+    if (pwLower == true && pwUpper == true && pwNum == true && pwSpecial == true) {
+      console.log("Scenario 1: lowercase, uppercase, numbers, & special characters");     // Log in the console that console logs after this point are related to scenario 1
+      let pwArray = [0];    // Set empty array to hold the random characters as they're generated
+      for(var i = 0; i < pwLength; i++) {   // Set for loop to iterate and create random characters until desired pw length is met
+        let typeIndex = Math.floor(Math.random() * 4);   // Set a var & assign it to a random number 0-3, where 0 = lower, 1 = Upper, 2 = number, 3 = special char.
+        console.log("Random type selected: " + typeIndex);    // Log typeIndex randomly generated to console for checking/errors
+        if (typeIndex == 0) {   // Set conditional: if character type randomly selected is lowercase letters...
+          let iL = arrLetters[Math.floor(math.random() * 26)];    // Set a var to be whatever letter in the arrLetters array is at an index position that's randomly generated
+          pwArry[i] = iL;   // Assign current index position in the password array to the randomly generated and assigned lowercase letter.
+        } else if (typeIndex == 1) {    // Set conditional: else if character type randomly selected is uppercase letters...
+          let iU = arrLetters[Math.floor(Math.random() * 26)].toUpperCase();    // Set a var to be whatever letter in the arrLetters array is at an index position that's randomly generated (and then capitalize it)
+          pwArry[i] = iU;   // Assign current index position in the pw array to the randomly generated (and capitalized)and assigned uppercase letter          
+        } else if (typeIndex == 2) {    // Set conditional: else if character type randomly selected is numbers...
+          let iN = [Math.floor(Math.random() * 10)];    // Set a var to be a randomly generated number between 0 and 9.
+          pwArray[i] = iN;    // Assign current index position in the pw array to the randomly generated number.
+        } else {    // Final else conditional: if character type randomly selected is not the others (0-2) - and instead is 3 for special characters...
+          let iS = arrSpecial[Math.floor(Math.random() * 29)];    // Set a var to be whatever special character in the arrSpecial array is at the index position that's randomly generated
+          pwArray[i] = iS;  // Assign current index position in the pw array to the randomly generation and assigned special character
+        }
+        console.log("Character " + (i+1) + ": " + pwArray[i]);    // Log the selected character to the console (logged with each iteration of the for.. loop)
+      }
+      pwResult = pwArray.join("");     // After the for... loop is finished, set the original pwResult variable to be the all the characters in the pw array joined with no separation character (so 1 continuous string)
+      console.log("Final Password: " + pwResult);     // log the final password generated to the console
+      return pwResult;    // Return the final password result from the function to be assigned as the "password" variable in the writePassword() function
+    } // end of scenario 1
 
-    }
+  } // end of else     
+
+} //end of function
